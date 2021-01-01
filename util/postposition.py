3a5077219c19fa1e2bd2c2f3cf5ba65b3c01ffc7
Sch <junhg0211@gmail.com>
2021-01-01 15:24:52 +0900

    Update postposition.py

--- util/postposition.py
+++ util/postposition.py
@@ -6,6 +6,7 @@ def a_ya(string: str):
     elif string in '1234567890':
         return '아' if string in '136780' else '야'
     else:
+        # noinspection SpellCheckingInspection
         return '아' if string not in 'aeiouyw' else '야'
 
 
@@ -50,7 +51,7 @@ def euro(string: str):
     string = string[-1]
 
     if 44032 <= ord(string):
-        return '으로' if (ord(string) - 44032) % 28 else '로'
+        return '로' if (ord(string) - 44032) % 28 in [0, 8] else '으로'
     elif string in '1234567890':
         return '으로' if string in '360' else '로'
     else:
