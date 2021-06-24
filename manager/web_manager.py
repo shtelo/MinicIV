e36346a19020cd875c0a195a5fefbe13b7c3e759
Sch <junhg0211@gmail.com>
2021-06-24 18:13:08 +0900

    cors allowed for all routes ; as i'm trying to separate maze application to another project

--- manager/web_manager.py
+++ manager/web_manager.py
@@ -1,9 +1,11 @@
 from os.path import join
 
 from flask import Flask, send_file
+from flask_cors import CORS
 
 web_app = Flask(__name__, template_folder='../templates/')
 # logging.getLogger('werkzeug').setLevel(logging.ERROR)
+CORS(web_app)
 
 
 @web_app.route('/src/<path:path>')
